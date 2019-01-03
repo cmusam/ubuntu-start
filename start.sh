432a8f5e5b933e739ca1afa1daa58cb541c132fc
Yan Shen <samshens@yahoo.com>
2019-01-02 22:58:28 -1000

    fix directory issue

--- start.sh
+++ start.sh
@@ -13,6 +13,7 @@ sudo apt install python3-pip -y
 # Sample project https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html
 sudo apt-get install maven -y
 
+cd ~
 mvn archetype:generate \
     -DgroupId=com.mycompany.app \
     -DartifactId=my-app \
@@ -24,14 +25,13 @@ cd my-app
 mvn package
 java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App
 mvn exec:java -Dexec.mainClass="com.mycompany.app.App"
-cd ~
 
 # MySQL
 # Sample database https://dev.mysql.com/doc/employee/en/
+cd ~
 sudo apt-get install mysql-server -y
-
 git clone https://github.com/datacharmer/test_db.git
-sudo mysql -t < test_db/employees.sql
-sudo mysql -t < test_db/test_employees_sha.sql
-
+cd test_db
+sudo mysql -t < employees.sql
+sudo mysql -t < test_employees_sha.sql
 
